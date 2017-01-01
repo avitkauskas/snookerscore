0cff77968e20e40023d89b5e8a4d833e5d5d3496
Alvydas Vitkauskas <alvydas.vitkauskas@gmail.com>
2017-01-01 19:24:22 +0200

    Mobile login restored

--- imports/ui/layouts/header.js
+++ imports/ui/layouts/header.js
@@ -1,20 +1,5 @@
 import { Template } from 'meteor/templating';
 
 Template.Header.onRendered(function() {
-
   this.$('.dropdown').dropdown();
-
-  this.$('#mobile-menu').on("click", function(e) {
-    e.preventDefault();
-    $('#vertical-menu').toggle();
-  });
-
-  this.$('#all-matches').on("click", function(e) {
-    $('#vertical-menu').toggle();
-  });
-
-  this.$('#new-match').on("click", function(e) {
-    $('#vertical-menu').toggle();
-  });
-
 });
